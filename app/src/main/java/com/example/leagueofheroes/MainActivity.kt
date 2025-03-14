fad436187c48f55bd0d02c8b485228b519399367
IOSEdwin <eramirezn23@icloud.com>
2025-03-14 12:18:02 +0100

    starting the proyect super Heroes

--- app/src/main/java/com/example/leagueofheroes/MainActivity.kt
+++ app/src/main/java/com/example/leagueofheroes/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    //creo las variables para poder acceder a ellas desde cualquier parte del codigo
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+            // le doys valores a las variables accediento por el ID a sus respectivos componentes
+            initComponents()
+          //  initListeners()
+
+
         }
+    }
+
+
+
+    private fun initComponents() {
+        //variable=find..<[tipoDeComponente]>(direccion donde se encuentra el  comoponente ej: R.id. XXXX
+        recyclerView=findViewById<RecyclerView>(R.id.recyclerView)
+    }
+    private fun initListeners() {
+
     }
 }
---
+++ app/src/main/java/com/example/leagueofheroes/SuperHero.kt
@@ -0,0 +1,18 @@
+package com.example.leagueofheroes
+// en esta clase creamos el modelo de datos de la respuesta de la API
+class SuperHeroResponse (
+    val response: String,
+    val results: List<SuperHero>
+    ) {
+
+    }
+// en esta clase creamos el modelo de datos de un super heroe
+    class SuperHero (
+        val id: String,
+        val name: String,
+        val image: Image
+    ) {
+
+    }
+// en esta clase creamos el modelo de datos de la imagen osea la URL de la imagen
+    class Image (val url: String)
